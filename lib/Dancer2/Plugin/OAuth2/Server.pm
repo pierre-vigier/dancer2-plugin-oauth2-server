5f590892e2d9f3631647aae883a9c5e9d2a23e0f
Pierre VIGIER <pierre.vigier@gmail.com>
2015-12-05 12:39:43 +0800

    0.02

--- lib/Dancer2/Plugin/OAuth2/Server.pm
+++ lib/Dancer2/Plugin/OAuth2/Server.pm
@@ -3,7 +3,7 @@ package Dancer2::Plugin::OAuth2::Server;
 use strict;
 use warnings;
 use 5.008_005;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 use Dancer2::Plugin;
 use URI;
 use URI::QueryParam;
